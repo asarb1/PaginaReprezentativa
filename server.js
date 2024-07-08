3c78bc3ff2f42650836cd218e087f791ddcc8e68
Andreea MacBook <andreeamacbook@Andreeas-MacBook.local>
2024-07-08 17:19:47 +0300

    navbar

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const session = require("express-session");
+const app = express()
+const port = 3000
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false,
+}));
+
+app.get('/', (req, res) => {
+  res.render('mainpage')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
